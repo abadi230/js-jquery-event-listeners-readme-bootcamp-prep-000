ba8afa54f26cc3dfe1f4cf42fb73fd949f3ef5aa
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-18 12:57:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -28,7 +28,7 @@ function pressIt(){
 function submitIt(){
   $('form').on('submit', ()=>{
     if($("input:first").val() === 'g'){
-       alert("Your form is going to be submitted now");
+       alert('Your form is going to be submitted now.');
        return;
     }
     
